bcc7fb5ff5733e28d4256ba5f8cad73924a2299f
elusiven <elusiven@me.com>
2016-10-06 17:28:37 +0100

    adding new features

--- admin/upload.php
+++ admin/upload.php
@@ -1,6 +1,38 @@
 <?php include("includes/header.php"); ?>
     <?php include("includes/top_nav.php"); ?>
          <?php include("includes/side_nav.php"); ?>
+            
+
+<?php
+
+$message = "";
+
+if(isset($_POST['submit'])) {
+    
+    $photo = new Photo();
+    $photo->title = $_POST['title'];
+    $photo->description = $_POST['description'];
+    $photo->set_file($_FILES['file_upload']);
+    
+    if($photo->save()) {
+        
+        $message = "Photo Uploaded Successfuly";
+    } else {
+        
+        $message = join("<br>", $photo->errors);
+    }
+}
+
+
+
+
+
+?>
+            
+            
+            
+            
+             
         </nav>
 
     <!-- END OF HEADER AND NAVIGATION --> 
@@ -23,6 +55,29 @@
                                 <i class="fa fa-file"></i> Blank Page
                             </li>
                         </ol>
+                        
+                        <div class=""></div>
+
+                       <div class="col-md-6">
+                        <form action="upload.php" method="post" enctype="multipart/form-data">
+                            
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input type="text" name="title" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <input type="text" name="description" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="file_upload">Upload an image</label>
+                                <input type="file" name="file_upload">
+                            </div>
+                            
+                            <input type="submit" name="submit" class="btn btn-success">                            
+                            
+                        </form>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
