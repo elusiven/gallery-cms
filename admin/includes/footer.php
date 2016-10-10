8b19e87787ef1d2e1132ea9f3273210aa2606b1b
elusiven <elusiven@me.com>
2016-10-10 16:38:25 +0100

    new features

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,11 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
     
+    <!-- WYSIWIG TEXT EDITOR FOR ADMIN -->
+    
+    <script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script src="js/scripts.js"></script>
+
     
 
 </body>
